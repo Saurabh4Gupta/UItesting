9be10ea289db51153d045bf4b6519d1b7ad8d75d
Manish Kumar <manish.kumar@manishkumar.successivetech.com>
2020-11-04 11:05:01 +0530

    pipeline setup

--- brigade.js
+++ brigade.js
@@ -1,251 +1,96 @@
-const { events, Job, Group } = require("brigadier")
-const { BuildTaskFactory, DeployTaskFactory, ApprovalTaskFactory, PackageTaskFactory, Events, NotifyInfoJob } = require("devops-brigade")
+const { Job, Group } = require('brigadier');
+const devops = require('devops-brigade');
 
+async function getValues(teamEnv, project){
+  const env1 = devops.Utilities.getCoreEnvFromTeamEnv(teamEnv);
+  const values = {
+    node_env: `${project.secrets[`${teamEnv}_node_env`]}`,
+    is_bypass_security: `${project.secrets[`${teamEnv}_is_bypass_security`]}`,
+    port: 80,
+    teamEnv,
+    image: {
+      tag: '${APP_VER}',
+      repository: `${project.secrets.app_container_reg}/${project.secrets.app_name}`,
+    },
+    okta_issuer: project.secrets[`${teamEnv}_okta_issuer`],
+    client_id: project.secrets[`${teamEnv}_client_id`],
+    skip_preflight_true: project.secrets[`${teamEnv}_skip_preflight_true`],
+  };
 
-class JobFactory {
-
-  createBuildJob(e, project) {
-    // TODO: If not "node", specify alternative docker container for your build
-    var build = new Job("build", "node:10.15.0-slim")
-    build.storage.enabled = true
-    build.timeout = 180000000;
-
-    let taskFactory = new BuildTaskFactory(e, project)
-
-    build.tasks = [
-      "cd /src",
-      "apt-get update && apt-get --assume-yes install git-core",
-      // "apt-get update && apt-get --assume-yes install p7zip-full",
-
-      taskFactory.gitVersion(),
-
-      // TODO: Remove npmVersion if NOT a node project
-      taskFactory.npmVersion(),
-      "git config --global credential.helper 'store --file ~/.git-credentials'",
-      `echo https://${project.secrets.giteauser}:${project.secrets.giteapass}@gitea-tooling.az.devops.gdpdentsu.net > ~/.git-credentials`,
-      `echo \n >> ~/.git-credentials`,
-      `echo https://${project.secrets.giteauser}:${project.secrets.giteapass}@gitea-tooling.az.connectors.gdpdentsu.net >> ~/.git-credentials`,
-      // "cat ~/.git-credentials",
-      "npm install",
-      "export SKIP_PREFLIGHT_CHECK=true",
-      "export GENERATE_SOURCEMAP=false",
-      "export NODE_OPTIONS=--max_old_space_size=4096",
-      // Build
-      "npm run build:prod",
-      // Lint
-      // "yarn lint",
-
-      // Unit test
-      // `yarn test:c || true`,
-      taskFactory.storeBuild()
-    ]
-    return build;
+  
+  if ((teamEnv.includes('-int-g1ga')) || (teamEnv.includes('-nft-g1ga')) || (teamEnv.includes('-stg-g1ga')) || (teamEnv.includes('-prod-g1ga')) || (teamEnv.includes('-demo-g1ga'))) {
+    values.apollo_graphql_uri =  `https://ga-api-gw.wal.${env1}.az.eu.mediaecosystem.io/prodman`,
+    values.apollo_subscription_uri = `wss://ga-api-gw.wal.${env1}.az.eu.mediaecosystem.io/prodman`;
+    values.ingress.hosts = [`productivity-ui.wal.${env1}.az.eu.mediaecosystem.io`];
+    values.ingress.class = "external";
+    values.ingress.tls = [{
+      secretName: `productivity-ui.wal.${env1}.az.eu.mediaecosystem.io`,
+      hosts: [
+        `productivity-ui.wal.${env1}.az.eu.mediaecosystem.io`,
+      ],
+    }, ];
   }
 
-  createUnitTestJob(e, project) {
-    var unitTest = new Job("unit-test", "node:10.15.0-slim")
-    unitTest.storage.enabled = true
-
-    let taskFactory = new TaskFactory(e, project)
-    unitTest.tasks = [
-      "rm -rf /src/*",
-      "cd /src",
-      taskFactory.retrieveBuild(),
-      "cat /src/test-report.xml  | grep -q '<failure' && { echo \"Unit tests failed\"; exit 2; } || { echo \"Unit tests passed\"; exit 0; }",
-    ]
-    return unitTest;
+  if ((teamEnv.includes('-brp-g1ga'))|| (teamEnv.includes('-pch-g1ga')) || (teamEnv.includes('-puat-g1ga')) || (teamEnv.includes('-pstg-g1ga'))) {
+    values.apollo_graphql_uri =  `https://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.apollo_subscription_uri = `wss://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.ingress.hosts = [`${teamEnv}-${project.secrets.app_name}.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`];
+    values.ingress.class = "external";
+    values.ingress.tls = [{
+      secretName: `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
+      hosts: [
+        `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
+      ],
+    }, ];
   }
 
-  createE2ETestJob(e, project) {
-    var e2eTest = new Job("e2e-test", "timbru31/java-node")
-    e2eTest.storage.enabled = true
-    e2eTest.timeout = 3600000;
-    let taskFactory = new TaskFactory(e, project)
-    e2eTest.tasks = [
-      "cd /src",
-      taskFactory.retrieveBuild(),
-      "apt-get update && apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget",
-      `export IBRP_PASS=${project.secrets.test_user_pass}`,
-      "npm install -g allure-commandline --save-dev",
-      // TODO testing: need to remove true
-      "npm run test:e2e > /mnt/brigade/share/e2e.log || true",
-      // "cat /mnt/brigade/share/e2e.log",
-      "npm run test:report-gen",
-      `mkdir /mnt/brigade/share/e2e_test_results`,
-      "cd /mnt/brigade/share/e2e_test_results",
-      `mkdir ${project.secrets.app_name}`,
-      // "cd /src/codecept-tests/reports",
-      "cd /src/allure-report/",
-      `cp -r ./* /mnt/brigade/share/e2e_test_results/${project.secrets.app_name}`
-    ]
-
-    return e2eTest;
+  if ((teamEnv.includes('-dint-g1ga'))) {
+    values.apollo_graphql_uri =  `https://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.apollo_subscription_uri = `wss://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.ingress.hosts = [`${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`];
+    values.ingress.class = "nginx";
+    values.ingress.tls = [{
+      secretName: `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`,
+      hosts: [
+        `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`,
+      ],
+    }, ];
   }
 
-  createUploadResultsJob(e, project) {
-    let job = new Job("upload-e2e-test-results", `microsoft/azure-cli`);
-    job.storage.enabled = true;
-    job.tasks = [
-      "export APP_VER=`cat /mnt/brigade/share/pipeline_app_version.txt | sed 's/d//g'`",
-      "echo $APP_VER",
-      `az login --service-principal -u ${project.secrets.azure_client_id} -p ${project.secrets.azure_client_secret} --tenant ${project.secrets.azure_tenant_id}`,
-      `az storage blob upload-batch --account-name gdpclientsaacc -s /mnt/brigade/share/e2e_test_results/${project.secrets.app_name} -d gdpclient01-bdd-results/${project.secrets.app_name}/$APP_VER --subscription ${project.secrets.bdd_result_subscription}`
-    ];
-    return job;
-  }
-  createE2ETestResultJob(e, project) {
-    var bddTest = new Job("e2e-test-result", `docker:dind`)
-    bddTest.storage.enabled = true
-    //TODO for time being as test were failing, so done exit 0 in first case it should be exit 2
-    bddTest.tasks = [
-      "cat /mnt/brigade/share/e2e.log  | grep -E 'FAIL|FAILURES' && { echo \"E2E tests failed\"; exit 0; } || { echo \"E2E tests passed\"; exit 0; }",
-    ]
-
-    return bddTest;
+  if ((teamEnv.includes('-hint-g1ga'))) {
+    values.apollo_graphql_uri =  `https://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.apollo_subscription_uri = `wss://${project.secrets[`${teamEnv}_sharedEnvGA`]}-kong-proxy.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net/prodman`;
+    values.ingress.hosts = [`${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`];
+    values.ingress.class = "nginx";
+    values.ingress.tls = [{
+      secretName: `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`,
+      hosts: [
+        `${teamEnv}-${project.secrets.app_name}.az.${project.secrets[`${teamEnv}-target_name`]}.gdpdentsu.net`,
+      ],
+    }, ];
   }
 
-  createStaticAnalysisJob(e, project) {
-    var staticAnalysis = new Job("static-analysis", "node:10.15.0-slim")
-    staticAnalysis.storage.enabled = true
-
-    let taskFactory = new TaskFactory(e, project)
-    staticAnalysis.tasks = [
-      // Using https://www.npmjs.com/package/jest-sonar-reporter
-      "npm install -g sonarqube-scanner",
-      "rm -rf /src/*",
-      "echo app is $APP_NAME",
-      "cd /src",
-      taskFactory.retrieveBuild(),
-      // https://github.com/gotwarlost/istanbul/pull/771
-      "sed -i 's|SF:/src/src|SF:src|g' coverage/lcov.info",
-      "sed -i 's|/src/src|src|g' test-report.xml",
-      `sonar-scanner -X -Dsonar.host.url=http://${project.secrets.team_name}-sonarqube-sonarqube:9000 -Dsonar.exclusions=src/index.ts,src/types/morgan-body.d.ts,src/tests/mocks/** -Dsonar.sources=src -Dsonar.login=${project.secrets.sonar_login} -Dsonar.password=${project.secrets.sonar_pass} -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info -Dsonar.testExecutionReportPaths=test-report.xml -Dsonar.tests=src -Dsonar.test.inclusions=src/**/*.spec.ts -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info`,
-    ]
-
-    return staticAnalysis;
-  }
-
-  createPackageJob(e, project) {
-    var packageJob = new Job("package", "docker:dind")
-    packageJob.storage.enabled = true
-    packageJob.privileged = true; // dind needs to run in privileged mode
-
-    packageJob.env = {
-      "DOCKER_DRIVER": "overlay"
-    }
-
-    let taskFactory = new PackageTaskFactory(e, project)
-    packageJob.tasks = [
-      "mkdir /package && cd /package",
-      taskFactory.retrieveBuild(),
-      taskFactory.dockerStart(),
-      taskFactory.dockerPackage(),
-    ]
-
-    return packageJob;
-  }
-  createDeployJob(teamEnv, e, project) {
-    let deployTaskFactory = new DeployTaskFactory(teamEnv, e, project)
-    let deployJob = new Job(`deploy-${teamEnv}`, `globaldevopsreg11.azurecr.io/builder:latest`)
-    deployJob.storage.enabled = true
-
-    let values = {
-      port: 80,
-      image: {
-        tag: "${APP_VER}",
-        repository: `${project.secrets.app_container_reg}/${project.secrets.app_name}`
-      },
-      cors_origin: "[]",
-      ingress: {
-        enabled: true,
-        hosts: [`${teamEnv}-${project.secrets.app_name}.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`]
-      },
-      cms_apollo_subscription_uri: `ws://${teamEnv}-dentsu-platform-server.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
-      cms_apollo_graphql_uri: `http://${teamEnv}-dentsu-platform-server.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
-      apollo_graphql_uri: `http://${teamEnv}-dentsu-platform-server.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
-      cors_origin: ``,
-      apollo_subscription_uri: `ws://${teamEnv}-dentsu-platform-server.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
-      okta_issuer: project.secrets[`${teamEnv}_okta_issuer`],
-      okta_redirect: `http://${teamEnv}-${project.secrets.app_name}.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`,
-      client_id: project.secrets[`${teamEnv}_client_id`],
-      tos_id: project.secrets[`${teamEnv}_tos_id`],
-      clientid: project.secrets[`${teamEnv}_clientid`],
-      react_app_client_name: project.secrets[`${teamEnv}_client_name`],
-      cms_url: `http://${teamEnv}-dentsu-cms.az.${project.secrets[teamEnv + "-target_name"]}.gdpdentsu.net`
-    };
-    if (teamEnv.endsWith("-int")) {
-      deployTaskFactory.addLink("E2E Tests", `https://gdpclient01-bdd-results-blobweb.az.gdpclient.gdpdentsu.net/${project.secrets.app_name}/$APP_SEMVER/index.html`);
-    }
-    deployJob.tasks = [
-      deployTaskFactory.loginToCluster(),
-      deployTaskFactory.setAppVerEnv(),
-
-      "cd /src/",
-      deployTaskFactory.helmUpgradeInstallCommand(
-        `${teamEnv}`,
-        `${teamEnv}-${project.secrets.app_name}`,
-        `./helm/${project.secrets.app_name}`,
-        values)
-    ]
-    return deployJob;
-  }
-
-
-  createApprovalJob(e, project) {
-    let approval = new Job("approval", "docker:dind")
-    approval.storage.enabled = true
-    approval.privileged = true; // dind needs to run in privileged mode
-
-    approval.env = {
-      "DOCKER_DRIVER": "overlay"
-    }
-
-    let approvalTaskFactory = new ApprovalTaskFactory(e, project)
-    approval.tasks = [
-      approvalTaskFactory.basicApproval()
-    ]
-
-    return approval;
-  }
+  return values;
 }
 
-events.on("push", (e, project) => {
-  let jobFactory = new JobFactory()
-  var jsonPayload = JSON.parse(e.payload);
-  console.log(e)
-  // Run relevant stages
-  if (e.type == 'push') {
-    if (jsonPayload.ref == "refs/heads/develop") {
-      Group.runEach([
-        jobFactory.createBuildJob(e, project),
-        // jobFactory.createStaticAnalysisJob(e, project),
-        // jobFactory.createUnitTestJob(e, project),
-        jobFactory.createPackageJob(e, project),
-        jobFactory.createDeployJob(`${project.secrets.team_name}-dev`, e, project),
-        new NotifyInfoJob(e, project, `Deployment`, `Deployment to ${project.secrets.team_name}-dev at branch ${e.revision.ref} commitID ${e.revision.commit} initiated`),
-        jobFactory.createApprovalJob(e, project),
-        jobFactory.createDeployJob(`${project.secrets.team_name}-test`, e, project),
-        new NotifyInfoJob(e, project, `Deployment`, `Deployment to ${project.secrets.team_name}-test at branch ${e.revision.ref} commitID ${e.revision.commit} initiated`),
-        jobFactory.createDeployJob(`${project.secrets.team_name}-int`, e, project),
-        new NotifyInfoJob(e, project, `Deployment`, `Deployment to ${project.secrets.team_name}-int at branch ${e.revision.ref} commitID ${e.revision.commit} initiated`),
-        jobFactory.createE2ETestJob(e, project),
-        jobFactory.createUploadResultsJob(e, project),
-        jobFactory.createE2ETestResultJob(e, project),
-      ])
-    } else if (jsonPayload.ref == "refs/heads/master") {
-      console.log("Ignoring Master Push")
-    } else {
-      Group.runEach([jobFactory.createBuildJob(e, project)])
-    }
-  } else if (e.type == 'pull_request') {
-    console.log("Ignoring PULL REQUEST")
-  }
-})
-
-Events.enableNotifyOnError();
-events.on("deploy", (e, project) => {
-  let jsonPayload = JSON.parse(e.payload);
-  new JobFactory().createDeployJob(jsonPayload.teamEnv, e, project).run()
-})
-
-exports.JobFactory = JobFactory
+devops.MEPipelineFactory.createJSPipeline({
+  deployValuesProvider: getValues,
+  useACR: true,
+  buildOptions: {
+    packageManager: "npm",
+    buildCommand: "npm run build:prod",
+  },
+  unitTestOptions: {
+   enabled: false,
+   command: "npm run test:c",
+   coverageReport: "coverage"
+ },
+  sonarOptions: {
+    enabled: false,
+  },
+  componentTestOptions: {
+   enabled: false,
+  },
+  meFunctions: ["ga"],
+  skipTest: true,
+});
