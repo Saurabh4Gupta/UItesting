65a13caedddaedbea971744d09fce36ded4fb3e0
rohit-nx <rohit.singh@dentsu.com>
2020-11-11 20:29:50 +0530

    updated package.json and shell.js

--- src/Shell.js
+++ src/Shell.js
@@ -1,7 +1,21 @@
 import React from 'react';
-// import PlatformShell from '@gdo-global-client-reporting/dentsu-platform-shell'
-
+import PlatformShell from '@gdo-global-client-reporting/dentsu-platform-shell'
+import { createBridge } from '@gdo-global-client-reporting/dentsu-platform-bridge'
+import config from './config/config';
 import App from './App';
 
-const Shell = () => <App />
+const Shell = () => {
+  const { isBypassSecurity, isEmbeddedApp } = config;
+  // const { changeLanguage } = useContext(CmsContext);
+  createBridge({
+    // localeChangeHandler: locale => changeLanguage(locale),
+    isBypassSecurity,
+    appOrigin: 'https://gdp-client01-brp-g1ga-media-ecosystem.az.brp.gdpdentsu.net',
+  });
+  return (
+    <PlatformShell cmsConfig={{ collectionName: '' }} showSideBar={false} isEmbeddedApp={isEmbeddedApp} appTitle="Productivity Manager">
+      <App />
+    </PlatformShell>
+  )
+}
 export default Shell;
