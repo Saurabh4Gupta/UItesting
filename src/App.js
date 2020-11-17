5970b8ee0b9f8023838c0094a7ac11302d1df9b9
rohit-nx <73185193+rohit-nx@users.noreply.github.com>
2020-11-17 18:03:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ApolloProvider } from 'react-apollo';
 import './app.css';
 import { useAuth } from '@gdo-global-client-reporting/dentsu-platform-shell'
 import Routes from './Routes';
-import client from './apollo';
+// import client from './apollo';
 
 const Application = () => {
   const { token } = useAuth();
