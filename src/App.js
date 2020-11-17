503cdff044dfe708b9efe8f0742bc8d7ba177333
rohit-nx <rohit.singh@dentsu.com>
2020-11-17 20:37:42 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { ApolloProvider } from 'react-apollo';
 import './app.css';
+import { useAuth } from '@gdo-global-client-reporting/dentsu-platform-shell'
 import Routes from './Routes';
+import client from './apollo';
 
-const Application = () => (
-  <Routes />
-);
+const Application = () => {
+  console.log('>>>>>>>>>>>>>', useAuth());
+  const n = localStorage.getItem('okta-token-storage');
+  console.log('<<<<<<<<<<<<<<<<<<<<', JSON.parse(n).idToken);
+  const { token } = useAuth();
+  return (
+    <ApolloProvider client={client(token)}>
+      <Routes />
+    </ApolloProvider>
+  )
+};
 export default Application;
--- src/Shell.js
+++ src/Shell.js
@@ -1,7 +1,26 @@
 import React from 'react';
-// import PlatformShell from '@gdo-global-client-reporting/dentsu-platform-shell'
-
+import PlatformShell, { CmsContext, useCMS } from '@gdo-global-client-reporting/dentsu-platform-shell'
+import { createBridge } from '@gdo-global-client-reporting/dentsu-platform-bridge'
+import config from './config/config';
 import App from './App';
 
-const Shell = () => <App />
+const Shell = () => {
+  const { isBypassSecurity, appOrigin, isEmbeddedApp } = config;
+  console.log('>>>>>>>>>>', CmsContext);
+  console.log('<<<<<<<<<<<<<<<<<<<<<', useCMS());
+  // const { changeLanguage } = useCMS();
+
+  createBridge({
+    // localeChangeHandler: locale => changeLanguage(locale),
+    isBypassSecurity,
+    appOrigin,
+  });
+
+  console.log('config>>>>>>', config);
+  return (
+    <PlatformShell isEmbeddedApp={isEmbeddedApp} appTitle="Productivity Manager">
+      <App />
+    </PlatformShell>
+  )
+}
 export default Shell;
