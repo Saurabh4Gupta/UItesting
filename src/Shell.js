b41966b68d6bd19fe8ee7e1bc1a9396dfb16b90b
rohit-nx <rohit.singh@dentsu.com>
2020-11-17 20:53:47 +0530

    remove platformshell

--- src/Shell.js
+++ src/Shell.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import PlatformShell, { CmsContext, useCMS } from '@gdo-global-client-reporting/dentsu-platform-shell'
-import { createBridge } from '@gdo-global-client-reporting/dentsu-platform-bridge'
-import config from './config/config';
+// import PlatformShell, { CmsContext, useCMS } from '@gdo-global-client-reporting/dentsu-platform-shell'
+// import { createBridge } from '@gdo-global-client-reporting/dentsu-platform-bridge'
+// import config from './config/config';
 import App from './App';
 
-const Shell = () => {
-  const { isBypassSecurity, appOrigin, isEmbeddedApp } = config;
-  console.log('>>>>>>>>>>', CmsContext);
-  console.log('<<<<<<<<<<<<<<<<<<<<<', useCMS());
-  // const { changeLanguage } = useCMS();
+// const { isBypassSecurity, appOrigin, isEmbeddedApp } = config;
+// console.log('>>>>>>>>>>', CmsContext);
+// console.log('<<<<<<<<<<<<<<<<<<<<<', useCMS());
+// const { changeLanguage } = useCMS();
+// createBridge({
+//   // localeChangeHandler: locale => changeLanguage(locale),
+//   isBypassSecurity,
+//   appOrigin,
+// });
+// console.log('config>>>>>>', config);
+const Shell = () => (
+  // <PlatformShell isEmbeddedApp={isEmbeddedApp} appTitle="Productivity Manager">
+  <App />
+  // </PlatformShell>
+)
 
-  createBridge({
-    // localeChangeHandler: locale => changeLanguage(locale),
-    isBypassSecurity,
-    appOrigin,
-  });
-
-  console.log('config>>>>>>', config);
-  return (
-    <PlatformShell isEmbeddedApp={isEmbeddedApp} appTitle="Productivity Manager">
-      <App />
-    </PlatformShell>
-  )
-}
 export default Shell;
