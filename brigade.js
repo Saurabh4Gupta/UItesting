9ab0df3869742022970fc65f0758768d791e1931
Manish Kumar <manish.kumar@manishkumar.successivetech.com>
2020-11-18 14:45:56 +0530

    new env added

--- brigade.js
+++ brigade.js
@@ -16,6 +16,7 @@ async function getValues(teamEnv, project){
     client_id: project.secrets[`${teamEnv}_client_id`],
     skip_preflight_check: project.secrets[`${teamEnv}_skip_preflight_check`],
     apporigin: project.secrets[`${teamEnv}_apporigin`],
+    flagit_is_embedded_app: project.secrets[`${teamEnv}_flagit_is_embedded_app`],
     use_method_to_fetch_perms: project.secrets[`${teamEnv}_use_method_to_fetch_perms`],
     ingress: {
       class: 'nginx',
