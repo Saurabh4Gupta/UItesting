8c9624be128bd8aefdce1d699b37cbb37d45e746
rohit-nx <rohit.singh@dentsu.com>
2020-11-18 06:47:01 +0530

    chnaged config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 const envVars = process.env;
+console.log('envaaaaa', envVars);
 const config = {
   uri: `${envVars.REACT_APP_APOLLO_GRAPHQL_URI}/graphql`,
   sUri: `${envVars.REACT_APP_APOLLO_SUBSCRIPTION_URI}/graphql`,
@@ -8,8 +9,8 @@ const config = {
     client_id: envVars.REACT_APP_CLIENT_ID,
   },
   cmsURL: envVars.REACT_APP_CMS_URL,
-  isBypassSecurity: envVars.REACT_APP_IS_BYPASS_SECURITY === 'true',
-  isEmbeddedApp: envVars.REACT_APP_FLAGIT_IS_EMBEDDED_APP === 'true',
+  isBypassSecurity: envVars.REACT_APP_IS_BYPASS_SECURITY,
+  isEmbeddedApp: envVars.REACT_APP_FLAGIT_IS_EMBEDDED_APP,
   appOrigin: envVars.REACT_APP_APPORIGIN,
 };
 export default config;
