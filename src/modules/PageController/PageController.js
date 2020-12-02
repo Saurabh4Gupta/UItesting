4209e296931cb8bcecf2ac2f43a5eabb9ed0d009
Saurabh Baghel <saurabh.bagel@dentsu.com>
2020-12-02 22:12:38 +0530

    change spelling

--- src/modules/PageController/PageController.js
+++ src/modules/PageController/PageController.js
@@ -67,7 +67,7 @@ const PageController = (props) => {
             )}
           </>
         )}
-        status={isCompleted ? 'Completed' : ''}
+        status={isCompleted ? 'Complete' : ''}
       >
         {children}
       </Page>
