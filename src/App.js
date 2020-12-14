788fdec8f56b74caabb25bee25548eed66ad7b37
rohit-nx <rohit.singh@dentsu.com>
2020-12-14 20:15:36 +0530

    upgrade platform shell

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './app.css';
+import { ApolloProvider } from 'react-apollo'
+import { useAuth } from '@gdo-global-client-reporting/dentsu-platform-shell'
 import Routes from './Routes';
+import apolloClient from './apollo/apolloClient';
 
-const Application = () => (
-  <Routes />
-);
+const Application = () => {
+  const { token } = useAuth();
+  return (
+    <ApolloProvider client={apolloClient(token)}>
+      <Routes />
+    </ApolloProvider>
+  )
+};
 export default Application;
